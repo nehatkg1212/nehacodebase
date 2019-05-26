c61ef4539bcdef8e762423fa47a16784b1ddadc6
Neha Jayaswal <nehasince1212@gmail.com>
2019-05-26 17:04:54 +0530

    Committing first java class files
    Hello world file

---
+++ BasicCoreJava/src/datatypesAndvariables/FirstJavaClass.java
@@ -0,0 +1,12 @@
+package datatypesAndvariables;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World test");
+		// TODO Auto-generated method stub
+
+	}
+
+}
